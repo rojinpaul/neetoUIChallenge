c454222c9c134cd60d3d7f63cdbd51c7b66d4857
Rojin Paul <rojin.paul@bigbinary.com>
2022-07-13 23:11:01 +0530

    Notes list UI

--- app/javascript/src/components/Dashboard/Notes/Table.jsx
+++ app/javascript/src/components/Dashboard/Notes/Table.jsx
@@ -1,33 +1,80 @@
 import React, { useState } from "react";
 
-import { Table as NeetoUITable } from "neetoui";
+import { MenuVertical, Clock } from "@bigbinary/neeto-icons";
+import { Typography, Dropdown, Avatar, Tag } from "neetoui";
 
-import { NOTES_TABLE_COLUMN_DATA } from "./constants";
 import EditNotePane from "./Pane/Edit";
 
-const Table = ({ setSelectedNoteIds, notes = [], fetchNotes }) => {
+const Table = ({ notes = [], fetchNotes }) => {
   const [showEditNote, setShowEditNote] = useState(false);
-  const [selectedNote, setSelectedNote] = useState({});
-
   return (
     <>
-      <div className="notes-table-height w-full">
-        <NeetoUITable
-          rowData={notes}
-          columnData={NOTES_TABLE_COLUMN_DATA}
-          onRowSelect={selectedRowKeys => setSelectedNoteIds(selectedRowKeys)}
-          onRowClick={(_, note) => {
-            setSelectedNote(note);
-            setShowEditNote(true);
-          }}
-          allowRowClick={true}
-        />
+      <div className="grid w-full gap-3">
+        {notes.map(note => (
+          <div className="box-border flex-col border-2 p-4" key={note.id}>
+            <div className="flex">
+              <div className="flex w-3/4">
+                <Typography
+                  component="h4"
+                  style="h4"
+                  weight="semibold"
+                  fontFamily="SF Pro Text"
+                >
+                  {note.title}
+                </Typography>
+              </div>
+              <div className="flex w-1/4 justify-end">
+                <Dropdown
+                  buttonProps={{
+                    icon: MenuVertical,
+                    style: "text",
+                  }}
+                  position="left-end"
+                >
+                  <li>Edit</li>
+                  <li>Delete</li>
+                </Dropdown>
+              </div>
+            </div>
+            <Typography
+              className="mb-4"
+              style="body3"
+              weight="normal"
+              fontFamily="SF Pro Text"
+            >
+              {note.description}
+            </Typography>
+            <hr className="mx" />
+
+            <div className="flex items-end p-2">
+              <div className="w-4/5">
+                <Tag label={note.tag} className=" font-normal" />
+              </div>
+              <div className="flex w-1/5 justify-end gap-1">
+                <Clock size={24} />
+                <Typography
+                  style="body3"
+                  weight="normal"
+                  fontFamily="SF Pro Text"
+                >
+                  created 2 hours ago
+                </Typography>
+                <Avatar
+                  onClick={function noRefCheck() {}}
+                  size="small"
+                  user={{
+                    name: note.contact,
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
       <EditNotePane
         showPane={showEditNote}
         setShowPane={setShowEditNote}
         fetchNotes={fetchNotes}
-        note={selectedNote}
       />
     </>
   );
--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 
+import { Search, Settings, Plus } from "@bigbinary/neeto-icons";
 import EmptyNotesListImage from "images/EmptyNotesList";
-import { Delete } from "neetoicons";
-import { Button, PageLoader } from "neetoui";
-import { Container, Header, SubHeader } from "neetoui/layouts";
+import { Button, PageLoader, Typography } from "neetoui";
+import { Container, Header, MenuBar } from "neetoui/layouts";
 
-import notesApi from "apis/notes";
 import EmptyState from "components/Common/EmptyState";
 
 import DeleteAlert from "./DeleteAlert";
@@ -19,6 +18,7 @@ const Notes = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedNoteIds, setSelectedNoteIds] = useState([]);
   const [notes, setNotes] = useState([]);
+  const [isSearchCollapsed, setIsSearchCollapsed] = useState(true);
 
   useEffect(() => {
     fetchNotes();
@@ -27,8 +27,24 @@ const Notes = () => {
   const fetchNotes = async () => {
     try {
       setLoading(true);
-      const { data } = await notesApi.fetch();
-      setNotes(data.notes);
+      const dummyNotes = [
+        {
+          id: 1,
+          title: "How to claim the warranty?",
+          description:
+            '"Are you getting my texts???" she texted to him. He glanced at it and chuckled under his breath. Of course he was getting them, but if he wasn\'t getting',
+          contact: "Ronald Richards",
+          tag: "Getting Started",
+        },
+        {
+          id: 2,
+          title: "How to Process Refund?",
+          description: "How to get the refund for damaged Order?",
+          contact: "Jacobs Jones",
+          tag: "Bugs",
+        },
+      ];
+      setNotes(dummyNotes);
     } catch (error) {
       logger.error(error);
     } finally {
@@ -41,62 +57,109 @@ const Notes = () => {
   }
 
   return (
-    <Container>
-      <Header
-        title="Notes"
-        actionBlock={
-          <Button
-            onClick={() => setShowNewNotePane(true)}
-            label="Add New Note"
-            icon="ri-add-line"
-          />
-        }
-        searchProps={{
-          value: searchTerm,
-          onChange: e => setSearchTerm(e.target.value),
-        }}
-      />
-      {notes.length ? (
-        <>
-          <SubHeader
-            rightActionBlock={
-              <Button
-                label="Delete"
-                icon={Delete}
-                onClick={() => setShowDeleteAlert(true)}
-                disabled={!selectedNoteIds.length}
-              />
-            }
-          />
+    <>
+      <MenuBar showMenu={true} title="Notes">
+        <MenuBar.Block label="All" count={13} active />
+        <MenuBar.Block label="Users" count={2} />
+        <MenuBar.Block label="Leads" count={7} />
+        <MenuBar.Block label="Visitors" count={4} />
+
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: Search,
+              onClick: () => setIsSearchCollapsed(!isSearchCollapsed),
+            },
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Segments
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Search
+          collapse={isSearchCollapsed}
+          onCollapse={() => setIsSearchCollapsed(true)}
+        />
+        <MenuBar.Block label="Europe" count={80} />
+        <MenuBar.Block label="Middle-East" count={60} />
+        <MenuBar.Block label="Asia" count={60} />
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: Settings,
+            },
+            {
+              icon: Plus,
+            },
+            {
+              icon: Search,
+            },
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Tags
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Block label="Sales" count={80} />
+        <MenuBar.Block label="Finance" count={60} />
+        <MenuBar.Block label="User Experience" count={60} />
+      </MenuBar>
+      <Container>
+        <Header
+          title="Notes"
+          actionBlock={
+            <Button
+              onClick={() => setShowNewNotePane(true)}
+              label="Add New Note"
+              icon="ri-add-line"
+            />
+          }
+          searchProps={{
+            value: searchTerm,
+            onChange: e => setSearchTerm(e.target.value),
+          }}
+          menuBarToggle={function noRefCheck() {}}
+        />
+        {notes.length ? (
           <Table
             setSelectedNoteIds={setSelectedNoteIds}
             notes={notes}
             fetchNotes={fetchNotes}
           />
-        </>
-      ) : (
-        <EmptyState
-          image={EmptyNotesListImage}
-          title="Looks like you don't have any notes!"
-          subtitle="Add your notes to send customized emails to them."
-          primaryAction={() => setShowNewNotePane(true)}
-          primaryActionLabel="Add New Note"
+        ) : (
+          <EmptyState
+            image={EmptyNotesListImage}
+            title="Looks like you don't have any notes!"
+            subtitle="Add your notes to send customized emails to them."
+            primaryAction={() => setShowNewNotePane(true)}
+            primaryActionLabel="Add New Note"
+          />
+        )}
+        <NewNotePane
+          showPane={showNewNotePane}
+          setShowPane={setShowNewNotePane}
+          fetchNotes={fetchNotes}
         />
-      )}
-      <NewNotePane
-        showPane={showNewNotePane}
-        setShowPane={setShowNewNotePane}
-        fetchNotes={fetchNotes}
-      />
-      {showDeleteAlert && (
-        <DeleteAlert
-          selectedNoteIds={selectedNoteIds}
-          onClose={() => setShowDeleteAlert(false)}
-          refetch={fetchNotes}
-          setSelectedNoteIds={setSelectedNoteIds}
-        />
-      )}
-    </Container>
+        {showDeleteAlert && (
+          <DeleteAlert
+            selectedNoteIds={selectedNoteIds}
+            onClose={() => setShowDeleteAlert(false)}
+            refetch={fetchNotes}
+            setSelectedNoteIds={setSelectedNoteIds}
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
